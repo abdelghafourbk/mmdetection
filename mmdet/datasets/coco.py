28af6f841c3128c9e090d9d8fcc0e27d57ac9c3f
Aegyptiacus11 <78727327+Aegyptiacus11@users.noreply.github.com>
2024-04-02 15:28:50 +0200

    Update coco.py

--- mmdet/datasets/coco.py
+++ mmdet/datasets/coco.py
@@ -143,7 +143,7 @@ class CocoDataset(BaseDetDataset):
             inter_h = max(0, min(y1 + h, img_info['height']) - max(y1, 0))
             if inter_w * inter_h == 0:
                 continue
-            if ann['area'] <= 0 or w < 1 or h < 1:
+            if w < 1 or h < 1:
                 continue
             if ann['category_id'] not in self.cat_ids:
                 continue
