b18f42beb232093bd343ae5f5c622cf2ff0d3d11
Aegyptiacus11 <78727327+Aegyptiacus11@users.noreply.github.com>
2024-04-02 23:48:20 +0200

    Update schedule_1x.py

--- configs/_base_/schedules/schedule_1x.py
+++ configs/_base_/schedules/schedule_1x.py
@@ -3,10 +3,6 @@ train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=12, val_interval=1)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 
-train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=12, val_interval=1)
-val_cfg = dict(type='ValLoop')
-test_cfg = dict(type='TestLoop')
-
 # learning rate
 param_scheduler = [
     dict(
