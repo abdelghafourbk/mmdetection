a7f7feec7676454f4bbbe4f8f8cf4701d3d8fa6d
Aegyptiacus11 <78727327+Aegyptiacus11@users.noreply.github.com>
2024-04-02 23:38:23 +0200

    Update myconfig.py

--- configs/myconfig.py
+++ configs/myconfig.py
@@ -79,3 +79,18 @@ model = dict(
                 type='Shared2FCBBoxHead',
                 # explicitly over-write all the `num_classes` field from default 80 to 5.
                 num_classes=1)))
+
+optimizer = dict(
+    _delete_=True,
+    type='AdamW',
+    lr=0.0001,
+    betas=(0.9, 0.999),
+    weight_decay=0.05,
+    paramwise_cfg=dict(
+        custom_keys={
+            'absolute_pos_embed': dict(decay_mult=0.),
+            'relative_position_bias_table': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.)
+        }))
+lr_config = dict(warmup_iters=1000, step=[8, 11])
+runner = dict(max_epochs=12)
