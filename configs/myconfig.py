d9ae631591291e5100c2d1cc372af66a2667a79d
Aegyptiacus11 <78727327+Aegyptiacus11@users.noreply.github.com>
2024-04-02 16:36:14 +0200

    Update myconfig.py

--- configs/myconfig.py
+++ configs/myconfig.py
@@ -8,17 +8,34 @@ classes = ('__background__', 'meteor')
 data_root='/kaggle/input/datameteors/meteor'
 backend_args = None
 
+train_pipeline = [  # Training data processing pipeline
+    dict(type='LoadImageFromFile', backend_args=backend_args),  # First pipeline to load images from file path
+    dict(
+        type='LoadAnnotations',  # Second pipeline to load annotations for current image
+        with_bbox=True,  # Whether to use bounding box, True for detection
+        with_mask=False,  # Whether to use instance mask, True for instance segmentation
+        poly2mask=False),  # Whether to convert the polygon mask to instance mask, set False for acceleration and to save memory
+    dict(
+        type='RandomFlip',  # Augmentation pipeline that flips the images and their annotations
+        prob=0.5),  # The probability to flip
+    dict(type='PackDetInputs')  # Pipeline that formats the annotation data and decides which keys in the data should be packed into data_samples
+]
+
 train_dataloader = dict(
     batch_size=2,
     num_workers=2,
+    persistent_workers=True,
+    sampler=dict(  # training data sampler
+        type='DefaultSampler',  # DefaultSampler which supports both distributed and non-distributed training. Refer to https://mmengine.readthedocs.io/en/latest/api/generated/mmengine.dataset.DefaultSampler.html#mmengine.dataset.DefaultSampler
+        shuffle=True),  # randomly shuffle the training data in each epoch
     dataset=dict(
         type=dataset_type,
         # explicitly add your class names to the field `metainfo`
         metainfo=dict(classes=classes),
         data_root=data_root,
         ann_file='coco_train.json',
-        data_prefix=dict(img='frames')
-        )
+        data_prefix=dict(img='frames'),
+        pipeline=train_pipeline)
     )
 
 val_dataloader = dict(
