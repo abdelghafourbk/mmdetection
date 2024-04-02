c4bc3a37e059633c1a8ad77676ce529e81cf128a
Aegyptiacus11 <78727327+Aegyptiacus11@users.noreply.github.com>
2024-04-02 15:01:13 +0200

    Update myconfig.py

--- configs/myconfig.py
+++ configs/myconfig.py
@@ -1,6 +1,6 @@
 
 # the new config inherits the base configs to highlight the necessary modification
-_base_ = './config/faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
+_base_ = 'faster_rcnn/faster_rcnn_r50_fpn_1x_coco.py'
 
 # 1. dataset settings
 dataset_type = 'CocoDataset'
