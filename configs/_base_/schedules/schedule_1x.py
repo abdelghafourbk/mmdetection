c29b015a8a18dbbaf5c914a5da003f9b737b43fb
Aegyptiacus11 <78727327+Aegyptiacus11@users.noreply.github.com>
2024-04-02 17:44:16 +0200

    Update schedule_1x.py

--- configs/_base_/schedules/schedule_1x.py
+++ configs/_base_/schedules/schedule_1x.py
@@ -1,5 +1,5 @@
 # training schedule for 1x
-train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=12, val_interval=1)
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=10, val_interval=1)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 
@@ -10,9 +10,9 @@ param_scheduler = [
     dict(
         type='MultiStepLR',
         begin=0,
-        end=12,
+        end=10,
         by_epoch=True,
-        milestones=[8, 11],
+        milestones=[6, 9],
         gamma=0.1)
 ]
 
