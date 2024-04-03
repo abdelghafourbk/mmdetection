c5f670be4a64087fb931796c4057cb61cc9c1ebf
Aegyptiacus11 <78727327+Aegyptiacus11@users.noreply.github.com>
2024-04-03 15:58:22 +0200

    Create FasterRCNNswinV0.py

---
+++ configs/FasterRCNNswinV0.py
@@ -0,0 +1,80 @@
+# the new config inherits the base configs to highlight the necessary modification
+_base_ = 'swin/faster-rcnn_r50_swin.py'
+
+# 1. dataset settings
+dataset_type = 'CocoDataset'
+classes = ('meteor')
+data_root='/kaggle/input/datameteors/meteor'
+backend_args = None
+
+train_pipeline = [  # Training data processing pipeline
+    dict(type='LoadImageFromFile', backend_args=backend_args),  # First pipeline to load images from file path
+    dict(
+        type='LoadAnnotations',  # Second pipeline to load annotations for current image
+        with_bbox=True,  # Whether to use bounding box, True for detection
+        with_mask=False,  # Whether to use instance mask, True for instance segmentation
+        poly2mask=False),  # Whether to convert the polygon mask to instance mask, set False for acceleration and to save memory
+    dict(
+        type='RandomFlip',  # Augmentation pipeline that flips the images and their annotations
+        prob=0.5), # The probability to flip
+    dict(type='PackDetInputs')  # Pipeline that formats the annotation data and decides which keys in the data should be packed into data_samples
+]
+
+train_dataloader = dict(
+    batch_size=2,
+    num_workers=2,
+    dataset=dict(
+        type=dataset_type,
+        # explicitly add your class names to the field `metainfo`
+        metainfo=dict(classes=classes),
+        data_root=data_root,
+        ann_file='coco_train.json',
+        data_prefix=dict(img='frames'),
+        pipeline=train_pipeline)
+    )
+
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    dataset=dict(
+        type=dataset_type,
+        test_mode=True,
+        # explicitly add your class names to the field `metainfo`
+        metainfo=dict(classes=classes),
+        data_root=data_root,
+        ann_file='coco_val.json',
+        data_prefix=dict(img='frames')
+        )
+    )
+
+val_evaluator = dict(  # Validation evaluator config
+    type='CocoMetric',  # The coco metric used to evaluate AR, AP, and mAP for detection and instance segmentation
+    ann_file=data_root + '/coco_val.json',  # Annotation file path
+    metric=['bbox'],  # Metrics to be evaluated, `bbox` for detection and `segm` for instance segmentation
+    format_only=False,
+    backend_args=backend_args)
+test_evaluator = val_evaluator  # Testing evaluator config
+'''
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    dataset=dict(
+        type=dataset_type,
+        test_mode=True,
+        # explicitly add your class names to the field `metainfo`
+        metainfo=dict(classes=classes),
+        data_root=data_root,
+        ann_file='test/annotation_data',
+        data_prefix=dict(img='test/image_data')
+        )
+    )
+'''
+# 2. model settings
+
+# explicitly over-write all the `num_classes` field from default 80 to 5.
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(
+                type='Shared2FCBBoxHead',
+                # explicitly over-write all the `num_classes` field from default 80 to 5.
+                num_classes=1)))
