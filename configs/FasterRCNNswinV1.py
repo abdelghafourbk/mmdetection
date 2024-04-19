9ccf1ec341252b6f1d58d9fc5b151267c01a2e1b
Abdelghafour BENKHELIFA <65504431+abdelghafourbk@users.noreply.github.com>
2024-04-20 00:28:53 +0100

    Update FasterRCNNswinV1.py

--- configs/FasterRCNNswinV1.py
+++ configs/FasterRCNNswinV1.py
@@ -18,7 +18,7 @@ train_pipeline = [  # Training data processing pipeline
 ]
 
 train_dataloader = dict(
-    batch_size=8,
+    batch_size=2,
     num_workers=2,
     dataset=dict(
         type=dataset_type,
